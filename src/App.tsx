07d8e04930b69d33fbbdb1eeb206ae772864dbff
Anton <nokker@gmail.com>
2022-11-23 19:52:24 +0900

    add input fields add invalidate query

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,48 @@ import "./index.scss";
 import { trpc } from "./trpc";
 import { httpBatchLink } from "@trpc/client";
 
-const AppContent = () => {
+const queryClient = new QueryClient();
+
+const AppContent = ({}) => {
   const messages = trpc.getMessages.useQuery();
+  const addMessage = trpc.addMessage.useMutation();
+
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onAdd = () => {
+    addMessage.mutate(
+      {
+        message,
+        user,
+      },
+      {
+        onSuccess: () =>
+          queryClient.invalidateQueries({ queryKey: ["getMessages"] }),
+      }
+    );
+  };
   return (
     <>
       <h1>Last 10 messages:</h1>
+      <div className="mt-10">
+        <input
+          type="text"
+          value={user}
+          name="user"
+          onChange={(e) => setUser(e.target.value)}
+          className="p-5 border-2 border-gray-300 rounded-lg w-full"
+          placeholder="User"
+        />
+        <input
+          type="text"
+          value={message}
+          name="message"
+          className="p-5 border-2 border-gray-300 rounded-lg w-full"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message"
+        />
+      </div>
       <ul>
         {messages.data?.map((message, i) => (
           <li key={i}>
@@ -18,12 +55,12 @@ const AppContent = () => {
           </li>
         ))}
       </ul>
+      <button onClick={onAdd}>Add message</button>
     </>
   );
 };
 
 const App = () => {
-  const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [httpBatchLink({ url: "http://localhost:8080/trpc" })],
